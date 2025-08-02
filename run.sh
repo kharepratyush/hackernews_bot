f9b86fe1e441c7925d76c67c0275932b4181a12d
Shivangi Srivastava <shivangi.juhi@gmail.com>
2025-08-02 17:24:29 +0400

    updates

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Navigate to your project folder
+cd /Users/shivangisrivastava/Downloads/Pratyush/hackernews_bot || exit
+
+# Activate the virtual environment
+source .venv/bin/activate
+
+# Run the Python script
+python bot.py
